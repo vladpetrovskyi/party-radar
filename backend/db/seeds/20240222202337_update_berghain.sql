2c4d5e4313ac828b89f9673e95ca2dbff6a66a08
Vlad Petrovskyi <v.petrovskyi98@gmail.com>
2024-02-24 14:13:00 +0100

    add reverse compatibility
    Signed-off-by: Vlad Petrovskyi <v.petrovskyi98@gmail.com>

--- backend/db/seeds/20240222202337_update_berghain.sql
+++ backend/db/seeds/20240222202337_update_berghain.sql
@@ -3,7 +3,8 @@
 BEGIN;
 
 INSERT INTO dialog_settings (id, name, image_id, columns_number, is_capacity_selectable)
-VALUES (8, 'Stall selection', null, 5, true)
+VALUES (8, 'Stall selection', null, 5, true),
+       (9, 'Stall selection', null, 2, true)
 ON CONFLICT (id) DO UPDATE SET name                   = excluded.name,
                                image_id               = excluded.image_id,
                                columns_number         = excluded.columns_number,
@@ -12,7 +13,7 @@ ON CONFLICT (id) DO UPDATE SET name                   = excluded.name,
 INSERT INTO location (id, root_location_id, name, emoji, element_type_id, enabled, dialog_settings_id,
                       on_click_action_id, parent_id)
 VALUES (99, 1, 'WC (Right)', '🤥➡️', 4, true, 8, 2, 2),
-       (101, 1, 'WC (Left)', '🤥⬅️', 4, true, 2, 2, 2)
+       (101, 1, 'WC (Left)', '🤥⬅️', 4, true, 9, 2, 2)
 ON CONFLICT (id) DO UPDATE SET name               = excluded.name,
                                emoji              = excluded.emoji,
                                enabled            = excluded.enabled,
@@ -29,10 +30,6 @@ WHERE id = 16;
 UPDATE location
 SET name = 'WC (Far)'
 WHERE id = 33;
-
-UPDATE dialog_settings
-SET columns_number = 2
-WHERE id = 2;
 
 INSERT INTO location (id, name, emoji, enabled, element_type_id, on_click_action_id, parent_id, root_location_id)
 VALUES (100, 'Chill Swing', '🎢', true, 4, 1, 2, 1)
@@ -137,7 +134,7 @@ BEGIN;
 
 DELETE
 FROM dialog_settings
-WHERE id = 8;
+WHERE id IN (8, 9);
 
 UPDATE dialog_settings
 SET columns_number = 3
@@ -168,80 +165,80 @@ SET name         = t.name,
 FROM (VALUES -- LEVEL 3:
              -- - Berghain
              -- - - Dance floor
-             (34, 'FL', 0, 15),
-             (35, 'ML', 0, 15),
-             (36, 'BL', 0, 15),
-             (37, 'FM', 1, 15),
-             (38, 'M', 1, 15),
-             (39, 'BM', 1, 15),
-             (40, 'FR', 2, 15),
-             (41, 'MR', 2, 15),
-             (42, 'BR', 2, 15),
+             (34, 'FL', 0, 15, null),
+             (35, 'ML', 0, 15, null),
+             (36, 'BL', 0, 15, null),
+             (37, 'FM', 1, 15, null),
+             (38, 'M', 1, 15, null),
+             (39, 'BM', 1, 15, null),
+             (40, 'FR', 2, 15, null),
+             (41, 'MR', 2, 15, null),
+             (42, 'BR', 2, 15, null),
 
              -- - - WC
-             (43, 'LL3', 0, 16),
-             (44, 'LL2', 0, 16),
-             (45, 'LL1', 0, 16),
-             (46, 'LR5', 1, 16),
-             (47, 'LR4', 1, 16),
-             (48, 'LR3', 1, 16),
-             (49, 'LR2', 1, 16),
-             (50, 'LR1', 1, 16),
-             (51, 'RR5', 2, 16),
-             (52, 'RR4', 2, 16),
-             (53, 'RR3', 2, 16),
-             (54, 'RR2', 2, 16),
-             (55, 'RR1', 2, 16),
+             (43, 'LL3', 0, 16, null),
+             (44, 'LL2', 0, 16, null),
+             (45, 'LL1', 0, 16, null),
+             (46, 'LR5', 1, 16, null),
+             (47, 'LR4', 1, 16, null),
+             (48, 'LR3', 1, 16, null),
+             (49, 'LR2', 1, 16, null),
+             (50, 'LR1', 1, 16, null),
+             (51, 'RR5', 2, 16, null),
+             (52, 'RR4', 2, 16, null),
+             (53, 'RR3', 2, 16, null),
+             (54, 'RR2', 2, 16, null),
+             (55, 'RR1', 2, 16, null),
 
              -- - Panorama Bar
              -- - - Dance floor
-             (56, 'FL', 0, 17),
-             (57, 'BL', 0, 17),
-             (58, 'FM', 1, 17),
-             (59, 'BM', 1, 17),
-             (60, 'FR', 2, 17),
-             (61, 'BR', 2, 17),
+             (56, 'FL', 0, 17, null),
+             (57, 'BL', 0, 17, null),
+             (58, 'FM', 1, 17, null),
+             (59, 'BM', 1, 17, null),
+             (60, 'FR', 2, 17, null),
+             (61, 'BR', 2, 17, null),
 
              -- - - WC
-             (62, 'BL1', 0, 21),
-             (63, 'BL2', 0, 21),
-             (64, 'FL1', 1, 21),
-             (65, 'FL2', 1, 21),
-             (66, 'FL3', 1, 21),
-             (67, 'FL4', 1, 21),
-             (69, 'FR1', 2, 21),
-             (70, 'FR2', 2, 21),
-             (71, 'FR3', 2, 21),
-             (72, 'FR4', 2, 21),
-             (74, 'BR1', 3, 21),
-             (75, 'BR2', 3, 21),
-             (76, 'BR3', 3, 21),
-             (77, 'BR4', 3, 21),
-             (78, 'BR5', 3, 21),
-             (79, 'BR6', 3, 21),
+             (62, 'BL1', 0, 21, null),
+             (63, 'BL2', 0, 21, null),
+             (64, 'FL1', 1, 21, null),
+             (65, 'FL2', 1, 21, null),
+             (66, 'FL3', 1, 21, null),
+             (67, 'FL4', 1, 21, null),
+             (69, 'FR1', 2, 21, null),
+             (70, 'FR2', 2, 21, null),
+             (71, 'FR3', 2, 21, null),
+             (72, 'FR4', 2, 21, null),
+             (74, 'BR1', 3, 21, null),
+             (75, 'BR2', 3, 21, null),
+             (76, 'BR3', 3, 21, null),
+             (77, 'BR4', 3, 21, null),
+             (78, 'BR5', 3, 21, null),
+             (79, 'BR6', 3, 21, null),
 
              -- - Säule
              -- - - WC
-             (80, 'L2', 0, 29),
-             (81, 'L1', 0, 29),
-             (82, 'R2', 1, 29),
-             (83, 'R1', 1, 29),
+             (80, 'L2', 0, 29, null),
+             (81, 'L1', 0, 29, null),
+             (82, 'R2', 1, 29, null),
+             (83, 'R1', 1, 29, null),
 
              -- - Wardrobe WC
              -- - - Close
-             (84, '1', 0, 32),
-             (85, '2', 0, 32),
-             (86, '3', 1, 32),
-             (87, '4', 1, 32),
-             (88, '5', 2, 32),
-             (89, '6', 2, 32),
+             (84, '1', 0, 32, null),
+             (85, '2', 0, 32, null),
+             (86, '3', 1, 32, null),
+             (87, '4', 1, 32, null),
+             (88, '5', 2, 32, null),
+             (89, '6', 2, 32, null),
 
              -- - - Distant
-             (90, 'L2', 0, 33),
-             (91, 'L1', 0, 33),
-             (92, 'R3', 1, 33),
-             (93, 'R2', 1, 33),
-             (94, 'R1', 1, 33)) as t(id, name, column_index, parent_id, row_index)
+             (90, 'L2', 0, 33, null),
+             (91, 'L1', 0, 33, null),
+             (92, 'R3', 1, 33, null),
+             (93, 'R2', 1, 33, null),
+             (94, 'R1', 1, 33, null)) as t(id, name, column_index, parent_id, row_index)
 WHERE l.id = t.id;
 
 COMMIT;
